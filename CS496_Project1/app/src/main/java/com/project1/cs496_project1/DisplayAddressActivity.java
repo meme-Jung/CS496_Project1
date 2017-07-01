6bc13802474fa873348456de752d0a0975cb07ea
prayanalog <prayanalog@gmail.com>
2017-07-01 17:05:23 +0900

    complete button a, now have to design or something else

--- CS496_Project1/app/src/main/java/com/project1/cs496_project1/DisplayAddressActivity.java
+++ CS496_Project1/app/src/main/java/com/project1/cs496_project1/DisplayAddressActivity.java
@@ -1,9 +1,6 @@
 package com.project1.cs496_project1;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.os.Build;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +11,9 @@ import java.util.ArrayList;
 
 public class DisplayAddressActivity extends AppCompatActivity {
 
+    // crawling contacts.
+    // combine name and phoneNumber and add the ArrayList
+    // finally return the ArrayList<String>
     public ArrayList<String> addContacts(){
         ArrayList<String> contacts = new ArrayList<>();
         Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
@@ -22,20 +22,15 @@ public class DisplayAddressActivity extends AppCompatActivity {
             String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
             contacts.add(name + " " + phoneNumber);
         }
-
         phones.close();
         return contacts;
     }
 
-
+    // when this show up create the ContentView and call addContacts() function to collect name and phone-number
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_address);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 100);
-            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
-        }
 
         ArrayAdapter<String> adapter = new ArrayAdapter(getApplicationContext(), R.layout.address_row, addContacts());
 
@@ -44,4 +39,6 @@ public class DisplayAddressActivity extends AppCompatActivity {
 
 
     }
+
+
 }
--- CS496_Project1/app/src/main/java/com/project1/cs496_project1/MainActivity.java
+++ CS496_Project1/app/src/main/java/com/project1/cs496_project1/MainActivity.java
@@ -7,25 +7,51 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;       // for permission checking final value
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
-        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** override onRequestPermissionResult for permission checking final value
+     * if requestCode is MY_PERMISSION_REQUEST_READ_CONTACTS check the grantResults it is granted or not
+     * if granted showAddress function call else show text No Permission */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showAddress(getCurrentFocus()); // permission was granted, yay! Do the contacts-related task you need to do.
+                }
+                else {      // permission denied, boo! Disable the functionality that depends on this permission.
+                    Toast.makeText(this, "No Permissions ", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other permissions this app might request
+        }
+    }
+
     /** Called when the user clicks the Send button */
     public void showAddress(View view) {
-        // insert abcbdufcoiasdcpiojjwedcowec
         Intent intent = new Intent(this, DisplayAddressActivity.class);
-        startActivity(intent);
-
+        // check there is the permission READ_CONTACTS if there is not show pop-up for request Permission, otherwise startActivity showAddress intent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        }
+        else {
+            startActivity(intent);
+        }
     }
+
+
     public void showGallery(View view){
         Intent intent = new Intent(this, getGallery.class);
         startActivity(intent);
