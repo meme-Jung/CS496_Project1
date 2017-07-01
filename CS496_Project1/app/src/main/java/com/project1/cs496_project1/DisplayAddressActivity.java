1cf6d4d25b1bd2788240a9dbe5066d660d531aa4
prayanalog <prayanalog@gmail.com>
2017-07-01 15:38:54 +0900

    button_a almost complete

---
+++ CS496_Project1/app/src/main/java/com/project1/cs496_project1/DisplayAddressActivity.java
@@ -0,0 +1,48 @@
+package com.project1.cs496_project1;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class DisplayAddressActivity extends AppCompatActivity {
+
+    public ArrayList<String> addContacts(){
+        ArrayList<String> contacts = new ArrayList<>();
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+        while (phones.moveToNext()) {
+            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            contacts.add(name + " " + phoneNumber);
+        }
+
+        phones.close();
+        return contacts;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_address);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 100);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+        }
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(getApplicationContext(), R.layout.address_row, addContacts());
+
+        ListView listview = (ListView) findViewById(R.id.listView1);
+        listview.setAdapter(adapter);
+
+
+    }
+}
--- CS496_Project1/app/src/main/java/com/project1/cs496_project1/MainActivity.java
+++ CS496_Project1/app/src/main/java/com/project1/cs496_project1/MainActivity.java
@@ -1,13 +1,29 @@
 package com.project1.cs496_project1;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /** Called when the user clicks the Send button */
+    public void showAddress(View view) {
+
+        Intent intent = new Intent(this, DisplayAddressActivity.class);
+        startActivity(intent);
+
     }
 }
