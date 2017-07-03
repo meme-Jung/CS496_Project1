b0afb0ff5a9548b7464bd05969dad6af9c5aac75
prayanalog <prayanalog@gmail.com>
2017-07-03 19:09:58 +0900

    button A is completed by customized adapter

---
+++ CS496_Project1/app/src/main/java/com/project1/cs496_project1/Contact.java
@@ -0,0 +1,13 @@
+package com.project1.cs496_project1;
+
+import android.net.Uri;
+
+/**
+ * Created by q on 2017-07-03.
+ */
+
+public class Contact {
+    public Uri profilePicture;
+    public String name;
+    public String phoneNumber;
+}
