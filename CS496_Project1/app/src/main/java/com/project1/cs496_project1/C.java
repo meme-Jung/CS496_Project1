b14f0bebfe3c03810d9018103b8c9ab0f43e0093
prayanalog <prayanalog@gmail.com>
2017-07-03 22:34:58 +0900

    fast change screen

--- CS496_Project1/app/src/main/java/com/project1/cs496_project1/C.java
+++ CS496_Project1/app/src/main/java/com/project1/cs496_project1/C.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -12,8 +13,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-public class C extends AppCompatActivity {
 
+public class C extends AppCompatActivity {
+    private Handler mHandler;
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +26,21 @@ public class C extends AppCompatActivity {
         super.onPause();
         overridePendingTransition(0, 0);
     }
+
     public void startgame(View view){
         Intent intent = new Intent(this, GetToBeClose.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        //       Log.i("startgame","1");
         startActivity(intent);
+   /*     mHandler = new Handler();
+ //       Log.i("startgame","2");
+     mHandler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+ //               Log.i("startgame","3");
+                finish();
+            }
+        },500);*/
         finish();
     }
 }
