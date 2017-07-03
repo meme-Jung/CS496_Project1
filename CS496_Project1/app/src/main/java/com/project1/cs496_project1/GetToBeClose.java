fca0d6f80293053c2a7a8d646ce5e957b8a97b95
Jay <tjrdnjs0313@kaist.ac.kr>
2017-07-03 19:43:50 +0900

    gettobeclose added

---
+++ CS496_Project1/app/src/main/java/com/project1/cs496_project1/GetToBeClose.java
@@ -0,0 +1,13 @@
+package com.project1.cs496_project1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetToBeClose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_to_be_close);
+    }
+}
