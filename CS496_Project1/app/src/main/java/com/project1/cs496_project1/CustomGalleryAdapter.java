4786ecc1ed20a1e8c72ebb97d8c5277b5bd3808f
Jay <tjrdnjs0313@kaist.ac.kr>
2017-07-04 21:04:11 +0900

    b finished

--- CS496_Project1/app/src/main/java/com/project1/cs496_project1/CustomGalleryAdapter.java
+++ CS496_Project1/app/src/main/java/com/project1/cs496_project1/CustomGalleryAdapter.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.ThumbnailUtils;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,42 +20,83 @@ import android.widget.GridView;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.SyncFailedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class CustomGalleryAdapter extends BaseAdapter {
     int CustomGalleryItemBg;
     Context mContext;
     String mBasePath = null;
+
+    String mBasePath2 = null;
     Bitmap bm;
+    String[] mImgList1;
+    String[] mImgList2;
     String[] mImgList;
     private LruCache mMemoryCache;
 
-    public CustomGalleryAdapter(Context c , String basepath) {
+    public CustomGalleryAdapter(Context c , String basepath, String basepath2) {
         mContext = c;
 
         this.mBasePath = basepath;
+        this.mBasePath2 = basepath2;
 
         File file = new File(mBasePath);
+        File file2 = new File(mBasePath2);
+        mImgList1 = file.list();
+        mImgList2 = file2.list();
+        int i;
+        final int memClass = ((ActivityManager) mContext.getSystemService(
+                Context.ACTIVITY_SERVICE)).getMemoryClass();
+        final int cacheSize = 1024 * 1024 * memClass/2;
+        mMemoryCache = new LruCache(cacheSize);
 
-        mImgList = file.list();
+        ImageView imageView;
+        imageView = new ImageView(mContext);
+        mImgList = new String[mImgList1.length+mImgList2.length];
+        for (i=0; i<mImgList2.length; i++)
+        {
+            if(mImgList2[i].contains("jpg")) {
+                mImgList2[i] = mBasePath2 + File.separator + mImgList2[i];
+                mImgList[i] = mImgList2[i];
+
+            }
+            else{
+
+            }
+        }
+        for (i=0; i<mImgList1.length; i++)
+        {
+            if(mImgList1[i].contains("jpg")) {
+
+                mImgList1[i] = mBasePath + File.separator + mImgList1[i];
+                mImgList[i + mImgList2.length] = mImgList1[i];
+            }
+            else{
+
+            }
+        }
         TypedArray array = mContext.obtainStyledAttributes(R.styleable.GalleryTheme);
         CustomGalleryItemBg = array.getResourceId(R.styleable.GalleryTheme_android_galleryItemBackground, 0);
         array.recycle();
+
+
+        //           BitmapFactory.Options options = new BitmapFactory.Options();
+//            options.inSampleSize = 4;
     }
 
     @Override
     public int getCount() {
-        File dir = new File(mBasePath);
-        mImgList = dir.list();
         return mImgList.length;
-
     }
     @Override
     public Object getItem(int position) {
         return position;
     }
     public String getItemPath(int position){
-        String path = mBasePath + File.separator + mImgList[position];
-        return path;
+        return mImgList[position];
     }
     @Override
     public long getItemId(int position) {
@@ -74,40 +116,54 @@ public class CustomGalleryAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
-//        Log.i("getview", String.valueOf(position));
-        if (convertView == null) {
+        Log.i("getView", String.valueOf(position));
+        String imageKey = String.valueOf(position);
+        Bitmap bm = getBitmapFromMemCache(imageKey);
+        imageView = new ImageView(mContext);
+        if (bm == null || convertView==null) {
+  //          Log.i("getview_null", String.valueOf(position));
             // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
             // Get memory class of this device, exceeding this amount will throw an
             // OutOfMemory exception.
-            final int memClass = ((ActivityManager) mContext.getSystemService(
-                    Context.ACTIVITY_SERVICE)).getMemoryClass();
+
 
             // Use 1/8th of the available memory for this memory cache.
-            final int cacheSize = 1024 * 1024 * memClass/4;
+//           final int cacheSize = 1024 * 1024 * memClass/4;
 
  //           mMemoryCache = new LruCache(cacheSize)
  //           String imageKey = mBasePath + File.separator + mImgList[position];
- //           Bitmap bm = getBitmapFromMemCache(imageKey);
+            //           Bitmap bm = getBitmapFromMemCache(imageKey);
 
  //           BitmapFactory.Options options = new BitmapFactory.Options();
 //            options.inSampleSize = 4;
     //        if(bm == null)
     //        {
-                bm = BitmapFactory.decodeFile(mBasePath + File.separator + mImgList[position]);
+                bm = BitmapFactory.decodeFile(mImgList[position]);
+                Bitmap mThumbnail = ThumbnailUtils.extractThumbnail(bm, 500, 550);
+                imageView.setPadding(8, 8, 8, 8);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.MATCH_PARENT, GridView.LayoutParams.MATCH_PARENT));
+
+                imageView.setRotation(90);
+            if(mThumbnail != null){
+                addBitmapToMemoryCache(imageKey, mThumbnail);
+
+            }
+                imageView.setImageBitmap(mThumbnail);
+   //         }
+   //         else{
+  //          }
+        }
+        else {/*
+                imageView = new ImageView(mContext);
+                bm = BitmapFactory.decodeFile(mImgList[position]);
                 Bitmap mThumbnail = ThumbnailUtils.extractThumbnail(bm, 500, 550);
                 imageView.setPadding(8, 8, 8, 8);
                 imageView.setRotation(90);
                 imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
                 imageView.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.MATCH_PARENT, GridView.LayoutParams.MATCH_PARENT));
-                imageView.setImageBitmap(mThumbnail);
-  //              addBitmapToMemoryCache(imageKey, mThumbnail);
-   //         }
-   //         else{
-                imageView.setImageBitmap(mThumbnail);
-  //          }
-        } else {
-            imageView = (ImageView) convertView;
+                imageView.setImageBitmap(mThumbnail);*/
+                imageView.setImageBitmap(bm);
         }
         return imageView;
     }
