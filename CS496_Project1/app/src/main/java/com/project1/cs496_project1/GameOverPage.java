3d80c2495a7051b646c1f7c79cf37aae3ff1207a
Jay <tjrdnjs0313@kaist.ac.kr>
2017-07-04 11:35:01 +0900

    gameoverpage finished

--- CS496_Project1/app/src/main/java/com/project1/cs496_project1/GameOverPage.java
+++ CS496_Project1/app/src/main/java/com/project1/cs496_project1/GameOverPage.java
@@ -1,7 +1,12 @@
 package com.project1.cs496_project1;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameOverPage extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class GameOverPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over_page);
     }
-
+    public void gotomain(View view){
+        finish();
+    }
+    public void retry(View view){
+        Intent intent = new Intent(this, GetToBeClose.class);
+        startActivity(intent);
+        finish();
+    }
 }
